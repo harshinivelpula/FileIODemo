170b7a5ae19ad9a6cfb8207508a471d2f2d38d55
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-10 15:03:59 +0530

    serialization,deserialization

---
+++ BinarySerialization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+    internal class BinarySerialization
+    {
+        public void Seralization()
+        {
+            Demo Sample = new Demo();
+            FileStream fileStream = new FileStream(@"C:\Users\DELL\source\repos\FileIODemo\FileIODemo\Demo.txt", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fileStream, Sample);
+
+        }
+
+        public void DeSeralization()
+        {
+            Demo Sample = new Demo();
+            FileStream fileStream = new FileStream(@"C:\Users\DELL\source\repos\FileIODemo\FileIODemo\Demo.txt", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Demo deserializationsampledemo = (Demo)formatter.Deserialize(fileStream);
+            Console.WriteLine("ApllicationName:" + deserializationsampledemo + "ApplicationID:" + deserializationsampledemo.ApplicationId);
+
+        }
+
+
+        [Serializable]
+        public class Demo
+        {
+            public string ApplicationName { get; set; } = "Binary Serialize";
+            public int ApplicationId { get; set; } = 1001;
+        }
+    }
+}
