a8e78027afde7eea0443f67cd6363779654f135b
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-13 17:12:59 +0530

    JSON

---
+++ JSONSerialization.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+     class JSONSerialization
+    {
+        
+
+        public void JsonSerialize()
+        {
+            BlogSites bsObj = new BlogSites()
+            {
+                Name = "sam",
+                Description = "welcome to universe"
+            };
+        string jsonData = JsonConvert.SerializeObject(bsObj);
+        Console.WriteLine(jsonData);
+        }
+    }
+[DataContract]
+class BlogSites
+{
+    [DataMember]
+    public string Name { get; set; }
+    [DataMember]
+    public string Description { get; set; }
+}
+
+
+
+    class JSONDeSerialization
+    {
+
+
+        public void JsonDeSerialize()
+        {
+            string json = @"{
+                  Name:'Sam',
+                 Description:'welcome to universe'
+
+             }";
+            BlogSites bsObj = JsonConvert.DeserializeObject<BlogSites>(json);
+            Console.WriteLine(bsObj.Name);
+        }
+    }
+    
+    
+}
